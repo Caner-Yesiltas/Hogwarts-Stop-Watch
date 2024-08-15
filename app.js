9cd577a4fd9c83a460e0041e1a0f9b1ab1169abc
CanerYesiltas <caneryesiltas1@gmail.com>
2024-08-15 09:54:42 -0400

    get all dom elements

---
+++ app.js
@@ -0,0 +1,4 @@
+const timerEl = document.getElementById('timer');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
